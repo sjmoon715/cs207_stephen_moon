c17d4cc3e266b35944a375ed270b51371a36a1b4
Stephen Moon <stephenmoon@dhcp-10-250-113-177.harvard.edu>
2019-09-12 18:53:50 -0400

    removed extra HW1-final from HW1-dev branch

--- HW1-final/p2.sh
+++
@@ -1,8 +0,0 @@
-grep -ic '\d' apollo13.txt > apollo_out.txt
-
-man grep | col -b | grep -A 1 -e "-c, --c"
-
-ls | grep -i ".*\.py$" | wc -l
-
-ls -l | grep -icv "......rw."
-
--- HW1-final/p2E.sh
+++
@@ -1,6 +0,0 @@
-for f in $( ls); do
-	if [ -f $f ]; then
-		lines=$(wc -l < $f)
-		echo $f $lines 	
-	fi
-done
